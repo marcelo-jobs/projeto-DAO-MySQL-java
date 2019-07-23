0f32badf71ceb472468e76d85ee8ab2539e64164
Marcelo Rufino <marcelo.orb@hotmail.com>
2019-07-23 15:09:24 -0300

    Implementado FindById

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,11 +11,12 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department dp = new Department(1, "Livros");
-		Seller sl = new Seller(1, "Maria", "maria@gmail.com", new Date(), 3.000, dp);
-		System.out.println(sl);
+	
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
+
+		System.out.println(seller);
 
 	}
 
