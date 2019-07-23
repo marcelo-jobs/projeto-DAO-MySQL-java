5434517f3f99580219e279dcaed951e4aa5a4187
Marcelo Rufino <marcelo.orb@hotmail.com>
2019-07-23 11:25:04 -0300

    Criada as Interfaces DepartmentDao e SellerDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department dp = new Department(1, "Livros");
-		System.out.println(dp);
+		Seller sl = new Seller(1, "Maria", "maria@gmail.com", new Date(), 3.000, dp);
+		System.out.println(sl);
 
 	}
 
