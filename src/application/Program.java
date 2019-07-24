d503814b87fc168b02043b3f31d8aa49bada0bf1
Marcelo Rufino <marcelo.orb@hotmail.com>
2019-07-24 09:37:59 -0300

    Adicionado findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,13 +11,19 @@ public class Program {
 
 	public static void main(String[] args) {
 
-	
 		System.out.println("=== TEST 1: seller findById =====");
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
 
 		System.out.println(seller);
+
+		System.out.println("\n=== TEST 2: seller findByDepartment =====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
